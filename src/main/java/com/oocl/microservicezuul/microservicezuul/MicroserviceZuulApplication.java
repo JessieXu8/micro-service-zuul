754bb70d42b7dc72bc191206fb36f734812093ef
Jessie Xu <jessie.xu@oocl.com>
2019-03-19 18:57:17 +0800

    initial zuul config

---
+++ src/main/java/com/oocl/microservicezuul/microservicezuul/MicroserviceZuulApplication.java
@@ -0,0 +1,15 @@
+package com.oocl.microservicezuul.microservicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class MicroserviceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceZuulApplication.class, args);
+    }
+
+}
